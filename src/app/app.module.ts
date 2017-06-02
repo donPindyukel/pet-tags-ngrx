50d1e21b98a3da1e29f6b37e239c61f5317fda57
Andrey Pindyuk <pindyuk@azoft.com>
2017-06-02 16:58:20 +0700

    Commit init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { StoreModule } from '@ngrx/store';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './core/app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateComponent } from './pages/create/create.component';
+import { CompleteComponent } from './pages/complete/complete.component';
+
+import { petTagReducer } from './core/pet-tag.reducer';
+import { AppRoutingModule } from './core/app-routing.module';
+import { TagShapeComponent } from './pages/create/tag-shape/tag-shape.component';
+import { TagTextComponent } from './pages/create/tag-text/tag-text.component';
+import { TagExtrasComponent } from './pages/create/tag-extras/tag-extras.component';
+import { TagPreviewComponent } from './tag-preview/tag-preview.component';
+import { AuthService } from './core/auth.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CreateComponent,
+    CompleteComponent,
+    TagShapeComponent,
+    TagTextComponent,
+    TagExtrasComponent,
+    TagPreviewComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule,
+    StoreModule.provideStore({ petTag: petTagReducer })
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
